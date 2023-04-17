f109cac0ff93117b0c49df2cddcce7e041c2c7c9
Humberto Moura <humbertomoura.n@gmail.com>
2023-04-17 15:05:57 -0300

    first project

---
+++ 1-projeto-basico/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("~/spring-radar-380614-013a70eac692.json")
+
+  project = "spring-radar-380614"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network-humberto"
+}
